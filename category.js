c995a9f4c3f740e0736f3a4e55f86da68eeb08fd
MudiaZuwa <94544783+MudiaZuwa@users.noreply.github.com>
2021-12-09 13:51:35 +0100

    Testfile

--- category.js
+++ category.js
@@ -3,3 +3,51 @@ async function button(){
 alert("file")
 await window.showOpenFilePicker()
 }
+export default async (options = [{}]) => {
+  if (!Array.isArray(options)) {
+    options = [options];
+  }
+  options[0].recursive = options[0].recursive || false;
+  return new Promise((resolve, reject) => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.webkitdirectory = true;
+
+    const _reject = () => cleanupListenersAndMaybeReject(reject);
+    const _resolve = (value) => {
+      if (typeof cleanupListenersAndMaybeReject === 'function') {
+        cleanupListenersAndMaybeReject();
+      }
+      resolve(value);
+    };
+    // ToDo: Remove this workaround once
+    // https://github.com/whatwg/html/issues/6376 is specified and supported.
+    const cleanupListenersAndMaybeReject =
+      options[0].legacySetup &&
+      options[0].legacySetup(_resolve, _reject, input);
+
+    input.addEventListener('change', () => {
+      let files = Array.from(input.files);
+      if (!options[0].recursive) {
+        files = files.filter((file) => {
+          return file.webkitRelativePath.split('/').length === 2;
+        });
+      } else if (options[0].recursive && options[0].skipDirectory) {
+        files = files.filter((file) => {
+          const directoriesName = file.webkitRelativePath.split('/');
+          return directoriesName.every(
+            (directoryName) =>
+              !options[0].skipDirectory({
+                name: directoryName,
+                kind: 'directory',
+              })
+          );
+        });
+      }
+
+      _resolve(files);
+    });
+
+    input.click();
+  });
+};
